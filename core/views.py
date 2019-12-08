4d8527a2f65d5e0a4ade3ce158f348f6d40c2ad1
Vlad Dmitrievich <me@2tunnels.com>
2019-12-08 14:58:32 +0200

    add home view + update url settings

--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+import random
 
-# Create your views here.
+from django.views.generic import TemplateView
+
+
+class HomeView(TemplateView):
+    template_name = 'core/home.html'
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['image'] = random.choice([
+            'https://media.giphy.com/media/QOYjGrYSuBMAg/giphy.gif',
+            'https://media.giphy.com/media/9Si6HlM5DfsA0/giphy.gif',
+            'https://media.giphy.com/media/pejdqIYGcxHMY/giphy.gif',
+        ])
+
+        return context_data
